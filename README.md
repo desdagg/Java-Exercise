40fd62bb2a292a476c8bf810fccb69d4a22ce79c
Des <desdagg@gmail.com>
2020-11-15 17:17:55 +0000

    Included jar file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Java-Exercise
+
+## compiled using maven
+
+### mvn package -DskipTests
+
+#### currently no tests
+
+## run the target jar
+
+### java -jar <jar file>
+
+## enter numbers for corresponding menu options and follow prompts
--- src/test/java/com/des/crudTest/crudTest/CrudTestApplicationTests.java
+++ src/test/java/com/des/crudTest/crudTest/CrudTestApplicationTests.java
@@ -1,13 +1,31 @@
 package com.des.crudTest.crudTest;
 
+import com.des.crudTest.crudTest.model.Person;
+import com.des.crudTest.crudTest.repository.PersonRepository;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
+
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class CrudTestApplicationTests {
 
+	@Resource
+	private PersonRepository personRepository;
+
+
 	@Test
 	void contextLoads() {
+
+		Person person = personRepository.save(new Person("personTest"));
+		Long id = person.getId();
+
+		Person foundPerson = personRepository.findById(id).get();
+		assertNotNull(foundPerson);
+		assertEquals(foundPerson.getFirst_name(), person.getFirst_name());
 	}
 
 }
